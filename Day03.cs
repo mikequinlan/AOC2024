7749b7696e42c87ae1c39b067c08e66454ef1e6e
Michael Quinlan <mquinlan@gmail.com>
2024-12-04 09:16:57 -0700

    Add files via upload

---
+++ Day03.cs
@@ -0,0 +1,54 @@
+﻿    using System.Text.RegularExpressions;
+
+    namespace AOC;
+
+    internal static class Program
+    {
+        private const string InputFile = "../../../Data/Data.txt";
+        private static readonly Regex Regex1 = new(@"mul\(([0-9]{1,3}),([0-9]{1,3})\)", RegexOptions.Multiline);
+        private static readonly Regex Regex2 = new(@"(mul\(([0-9]{1,3}),([0-9]{1,3})\))|(do\(\))|(don't\(\))", RegexOptions.Multiline);
+
+        private static void Main()
+        {
+            var input = File.ReadAllText(InputFile);
+            var part1 = Part1(input);
+            Console.WriteLine($"Part1: {part1}");
+            var part2 = Part2(input);
+            Console.WriteLine($"Part2: {part2}");
+        }
+
+        private static long Part1(string input)
+        {
+            var matches = Regex1.Matches(input);
+            var sum = 0;
+            foreach (Match match in matches)
+            {
+                var n1 = int.Parse(match.Groups[1].Value);
+                var n2 = int.Parse(match.Groups[2].Value);
+                sum += n1 * n2;
+            }
+            return sum;
+        }
+
+        private static long Part2(string input)
+        {
+            var matches = Regex2.Matches(input);
+            var sum = 0;
+            var enabled = true;
+            foreach (Match match in matches)
+            {
+                if (match.Captures[0].Value.StartsWith("mul"))
+                {
+                    if (enabled)
+                    {
+                        var n1 = int.Parse(match.Groups[2].Value);
+                        var n2 = int.Parse(match.Groups[3].Value);
+                        sum += n1 * n2;
+                    }
+                }
+                else if (match.Captures[0].Value.StartsWith("don't")) enabled = false;
+                else if (match.Captures[0].Value.StartsWith("do")) enabled = true;
+            }
+            return sum;
+        }
+    }
---
+++ Day04.cs
@@ -0,0 +1,78 @@
+﻿namespace AOC;
+
+internal static class Program
+{
+    private const string InputFile = "../../../Data/Data.txt";
+
+    private static void Main()
+    {
+        var input = File.ReadAllLines(InputFile);
+        var part1 = Part1(input);
+        Console.WriteLine($"Part1: {part1}");
+        var part2 = Part2(input);
+        Console.WriteLine($"Part2: {part2}");
+    }
+
+    private static long Part1(IReadOnlyList<string> input)
+    {
+        var count = 0;
+        for (int row = 0; row < input.Count; row++)
+        {
+            var line = input[row];
+            for (var col = 0; col < line.Length; col++)
+            {
+                if (line[col] == 'X') count += FindAllXMAS(input, row, col);
+            }
+        }
+        return count;
+    }
+
+    private static int FindAllXMAS(IReadOnlyList<string> input, int row, int col)
+    {
+        return MatchXMAS(input, row, col, -1, -1)
+               + MatchXMAS(input, row, col, -1, 0)
+               + MatchXMAS(input, row, col, -1, 1)
+               + MatchXMAS(input, row, col, 0, -1)
+               + MatchXMAS(input, row, col, 0, 1)
+               + MatchXMAS(input, row, col, 1, -1)
+               + MatchXMAS(input, row, col, 1, 0)
+               + MatchXMAS(input, row, col, 1, 1);
+    }
+    
+    private static int MatchXMAS(IReadOnlyList<string> input, int row, int col, int rowDelta, int colDelta)
+    {
+        return (MatchLetter(input, row + rowDelta, col + colDelta, 'M')
+                && MatchLetter(input, row + rowDelta * 2, col + colDelta * 2, 'A')
+                && MatchLetter(input, row + rowDelta * 3, col + colDelta * 3, 'S')) ? 1 : 0;
+    }
+
+    private static long Part2(IReadOnlyList<string> input)
+    {
+        var count = 0;
+        for (int row = 0; row < input.Count; row++)
+        {
+            var line = input[row];
+            for (var col = 0; col < line.Length; col++)
+            {
+                if (line[col] == 'A') count += MatchMAS(input, row, col);
+            }
+        }
+        return count;
+    }
+    
+    private static int MatchMAS(IReadOnlyList<string> input, int row, int col)
+    {
+        var match1 = (MatchLetter(input, row - 1, col - 1, 'M') && MatchLetter(input, row + 1, col + 1, 'S'))
+                     || (MatchLetter(input, row - 1, col - 1, 'S') && MatchLetter(input, row + 1, col + 1, 'M'));
+        var match2 = (MatchLetter(input, row + 1, col - 1, 'M') && MatchLetter(input, row - 1, col + 1, 'S'))
+                     || (MatchLetter(input, row + 1, col - 1, 'S') && MatchLetter(input, row - 1, col + 1, 'M'));
+        return match1 && match2 ? 1 : 0;
+    }
+
+    private static bool MatchLetter(IReadOnlyList<string> input, int row, int col, char letter)
+    {
+        if (row < 0 || row >= input.Count) return false;
+        if (col < 0 || col >= input[0].Length) return false;
+        return input[row][col] == letter;
+    }
+}
