2315ea3658aa424625c26ec0ff77d0d9a429bd32
Michael Quinlan <mquinlan@gmail.com>
2024-12-07 19:53:07 -0700

    Add files via upload

---
+++ Day06.cs
@@ -0,0 +1,182 @@
+﻿using System.Data;
+
+namespace AOC;
+
+[Flags]
+internal enum Dir
+{
+    None = 0,
+    Up = 0x01, 
+    Down = 0x02, 
+    Left = 0x04, 
+    Right = 0x08,
+    All = Up | Down | Left | Right,
+}
+
+internal readonly record struct Direction
+{
+    public static Direction Up => new(Dir.Up,-1, 0, "Up");
+    public static Direction Down => new(Dir.Down, 1, 0, "Down");
+    public static Direction Left => new(Dir.Left, 0, -1, "Left");
+    public static Direction Right => new(Dir.Right, 0, 1, "Right");
+    
+    public Dir Dir { get; }
+    public int RowDelta { get; }
+    public int ColDelta { get; }
+    public string Name { get; }
+
+    public override string ToString() => Name;
+
+    private Direction(Dir dir, int rowDelta, int colDelta, string name)
+    {
+        Dir = dir;
+        RowDelta = rowDelta;
+        ColDelta = colDelta;
+        Name = name;
+    }
+
+    public Direction Turn()
+    {
+        if (this == Up) return Right;
+        if (this == Down) return Left;
+        if (this == Right) return Down;
+        if (this == Left) return Up;
+        throw new InvalidOperationException();
+    }
+}
+
+internal static class Program
+{
+    private const string InputFile = "../../../Data/Data.txt";
+
+    private static void Main()
+    {
+        var input = File.ReadLines(InputFile).ToList();
+        var part1 = Part1(input);
+        Console.WriteLine($"Part1: {part1}");
+        var part2 = Part2(input);
+        Console.WriteLine($"Part2: {part2}");
+    }
+
+    private static long Part1(IReadOnlyList<string> input)
+    {
+        var (startRow, startCol, direction) = FindStart(input);
+        var data = GetData(input);
+
+        while (true)
+        {
+            data[startRow, startCol] = 'X';
+            var newRow = startRow + direction.RowDelta;
+            var newCol = startCol + direction.ColDelta;
+            var nextChar = GetValue(data, newRow, newCol);
+            if (nextChar == '*') break;
+            if (nextChar == '#')
+            {
+                direction = direction.Turn();
+                continue;
+            }
+            startRow = newRow;
+            startCol = newCol;
+        }
+
+        var count = 0;
+        for (var row = 0; row < data.GetLength(0); row++)
+        {
+            for (var col = 0; col < data.GetLength(1); col++)
+            {
+                if (data[row, col] == 'X') ++count;
+            }
+        }
+        return count;
+    }
+    
+    private static long Part2(IReadOnlyList<string> input)
+    {
+        var (startRow, startCol, direction) = FindStart(input);
+        var possibilities = new List<(int row, int col)>();
+        for (var row = 0; row < input.Count; row++)
+        {
+            for (var col = 0; col < input[0].Length; col++)
+            {
+                if (input[row][col] is '.') possibilities.Add((row, col));
+            }
+        }
+
+        var loopCount = 0;
+        foreach (var possibility in possibilities)
+        {
+            var data = GetData(input);
+            data[possibility.row, possibility.col] = '#';
+            var loop = MarkPath(data, startRow, startCol, direction);
+            if (loop) ++loopCount;
+        }
+        return loopCount;
+    }
+
+    private static char[,] GetData(IReadOnlyList<string> input)
+    {
+        var data = new char[input.Count, input[0].Length];
+        for (var row = 0; row < input.Count; row++)
+        {
+            for (var col = 0; col < input[0].Length; col++) data[row, col] = input[row][col];
+        }
+        return data;
+    }
+
+    private static Dir[,] GetVisited(char[,] data)
+    {
+        var visited = new Dir[data.GetLength(0), data.GetLength(1)];
+        for (var row = 0; row < data.GetLength(0); row++)
+        {
+            for (var col = 0; col < data.GetLength(1); col++) visited[row, col] = Dir.None;
+        }
+        return visited;
+    }
+
+    private static bool MarkPath(char[,] data, int startRow, int startCol, Direction direction)
+    {
+        var visited = GetVisited(data);
+        var row = startRow;
+        var col = startCol;
+        while (true)
+        {
+            var dir = visited[row, col];
+            if (dir.HasFlag(direction.Dir)) return true;
+            visited[row, col] |= direction.Dir;
+            var newRow = row + direction.RowDelta;
+            var newCol = col + direction.ColDelta;
+            var nextChar = GetValue(data, newRow, newCol);
+            if (nextChar == '*') break;
+            if (nextChar == '#')
+            {
+                direction = direction.Turn();
+                continue;
+            }
+            row = newRow;
+            col = newCol;
+        }
+        return false;
+    }
+
+    private static (int row, int col, Direction) FindStart(IReadOnlyList<string> input)
+    {
+        for (var row = 0; row < input.Count; row++)
+        {
+            for (var col = 0; col < input[0].Length; col++)
+            {
+                if (input[row][col] is '^') return (row, col, Direction.Up);
+                if (input[row][col] is 'v' or 'V') return (row, col, Direction.Down);
+                if (input[row][col] is '>') return (row, col, Direction.Right);
+                if (input[row][col] is '<') return (row, col, Direction.Left);
+            }
+        }
+        throw new ArgumentException("Invalid input");
+    }
+
+    private static char GetValue(char[,] data, int row, int col)
+    {
+        if (row is < 0 || row >= data.GetLength(0)) return '*';
+        if (col is < 0 || col >= data.GetLength(1)) return '*';
+        return data[row, col];
+    }
+}
---
+++ Day07.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace AOC;
+
+internal class Data
+{
+    public string Line { get; }
+    public long TestValue { get; }
+    public IReadOnlyList<long> Numbers { get; }
+    
+    public Data(string line)
+    {
+        Line = line;
+        var split = line.Split(": ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        TestValue = long.Parse(split[0]);
+        Numbers = split[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(long.Parse)
+            .ToList();
+    }
+}
+
+internal static class Program
+{
+    private const string InputFile = "../../../Data/Data.txt";
+
+    private static void Main()
+    {
+        var input = File.ReadLines(InputFile).Select(l => new Data(l)).ToList();
+        var part1 = Part1(input);
+        Console.WriteLine($"Part1: {part1}");
+        var part2 = Part2(input);
+        Console.WriteLine($"Part2: {part2}");
+    }
+
+    private static long Part1(List<Data> input)
+    {
+        long sum = 0;
+        foreach (var data in input)
+        {
+            if (IsPossiblyTrue(data)) sum += data.TestValue;
+        }
+        return sum;
+    }
+
+    private static bool IsPossiblyTrue(Data data)
+    {
+        var bitmapMax = 1L << (data.Numbers.Count - 1);
+        for (var bitmap = 0; bitmap < bitmapMax; bitmap++)
+        {
+            var operators = GetOperators(bitmap, data.Numbers.Count - 1).ToList();
+            if (IsTrue(data, operators)) return true;
+        }
+        return false;
+    }
+
+    private static bool IsPossiblyTrue2(Data data)
+    {
+        if (data.Numbers.Count > 31) throw new InvalidOperationException();
+        Console.WriteLine(data.Line);
+        var bitmapMax = 1L << ((data.Numbers.Count - 1) * 2);
+        for (var bitmap = 0; bitmap < bitmapMax; bitmap++)
+        {
+            var operators = GetOperators2(bitmap, data.Numbers.Count - 1).ToList();
+            if (IsTrue(data, operators)) return true;
+        }
+        return false;
+    }
+
+    private static IEnumerable<char> GetOperators(int number, int count)
+    {
+        var mask = 1L;
+        for (var i = 0; i < count; ++i)
+        {
+            yield return (number & mask) == 0 ? '+' : '*';
+            mask <<= 1;
+        }
+    }
+
+    private static IEnumerable<char> GetOperators2(int number, int count)
+    {
+        var mask = 3L;
+        for (var i = 0; i < count; ++i)
+        {
+            var test = number & mask;
+            if (test is 0L) yield return '+';
+            if (test is 1L) yield return '*';
+            if (test is 2L or 3L) yield return '|';
+            number >>= 2;
+        }
+    }
+
+    private static bool IsTrue(Data data, IReadOnlyList<char> operators)
+    {
+        var total = (BigInteger)data.Numbers[0];
+        for (var i = 1; i < data.Numbers.Count; i++)
+        {
+            switch (operators[i - 1])
+            {
+                case '+':
+                    total += data.Numbers[i];
+                    break;
+                case '*':
+                    total *= data.Numbers[i];
+                    break;
+                case '|':
+                    total = BigInteger.Parse(total.ToString() + data.Numbers[i]);
+                    break;
+            }
+        }
+        return data.TestValue == total;
+    }
+
+    private static long Part2(List<Data> input)
+    {
+        long sum = 0;
+        foreach (var data in input)
+        {
+            if (IsPossiblyTrue2(data)) sum += data.TestValue;
+        }
+        return sum;
+    }
+}
